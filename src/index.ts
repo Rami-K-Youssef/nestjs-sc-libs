4fa663ff859cc31df4665d5f24d2adaca0c14c09
Rami Kass Youssef <rami.k.youssef@gmail.com>
2022-01-10 16:04:10 +0200

    updates

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,15 @@ export * from "./transaction-helper";
 export * from "./mongoose-softdelete";
 export * from "./search-pagination";
 export * from "./backend-helpers";
+
+import { MongooseSoftDeleteModel } from "./mongoose-softdelete";
+import {
+  AccessibleFieldsDocument,
+  AccessibleFieldsModel,
+  AccessibleRecordModel,
+} from "@casl/mongoose";
+
+export type CompleteModel<T extends AccessibleFieldsDocument> =
+  AccessibleRecordModel<T> &
+    MongooseSoftDeleteModel<T> &
+    AccessibleFieldsModel<T>;
