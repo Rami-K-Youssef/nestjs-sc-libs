1554ef35b31e0a9c0767f50dac91a8f4c8f92e53
Rami Kass Youssef <rami.k.youssef@gmail.com>
2022-01-10 09:54:49 +0200

    added some exports

--- src/search-pagination/index.ts
+++ src/search-pagination/index.ts
@@ -1,3 +1,13 @@
 export * from "./search-key";
 export * from "./dto";
 export * from "./executor";
+export {
+  ISearchable,
+  ISearchableClass,
+  BaseCollectionProperties,
+  StampedCollectionProperties,
+  BaseResponseDto,
+  DiscriminatorDescDto,
+  PathOptions,
+  LookupFlags,
+} from "./definitions";
