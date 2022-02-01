570bac0a7d3c3eb50d67a82a946213e373d34b57
Rami Kass Youssef <rami.k.youssef@gmail.com>
2022-02-01 09:15:15 +0200

    bugfix

--- src/backend-helpers/decoractors.ts
+++ src/backend-helpers/decoractors.ts
@@ -19,7 +19,7 @@ export interface ClassTransformerOptionsExt extends ClassTransformOptions {
 }
 
 export function TransformDto(
-  dto: (...args: any[]) => unknown,
+  dto: new (...args: any[]) => unknown,
   options?: ClassTransformerOptionsExt,
   groupFn?: (item: any, user?: any) => string[]
 ) {
