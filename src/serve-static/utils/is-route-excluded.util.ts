992ae2666055268cc879cf33cf09a6c454c0a517
Rami Kass Youssef <rami.k.youssef@gmail.com>
2022-02-10 16:22:34 +0200

    bugfix

--- src/serve-static/utils/is-route-excluded.util.ts
+++ src/serve-static/utils/is-route-excluded.util.ts
@@ -1,15 +1,15 @@
-import * as pathToRegexp from 'path-to-regexp';
+import { pathToRegexp } from "path-to-regexp";
 
 export const isRouteExcluded = (req: any, paths: string[] = []) => {
   return paths.some((path) => {
     const re = pathToRegexp(path);
-    const queryParamsIndex = req.originalUrl.indexOf('?');
+    const queryParamsIndex = req.originalUrl.indexOf("?");
     const pathname =
       queryParamsIndex >= 0
         ? req.originalUrl.slice(0, queryParamsIndex)
         : req.originalUrl;
 
-    if (!re.exec(pathname + '/')) {
+    if (!re.exec(pathname + "/")) {
       return false;
     }
     return true;
