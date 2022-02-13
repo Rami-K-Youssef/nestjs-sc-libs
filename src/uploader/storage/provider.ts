6a7a597c173dd6f7384a678d8d56470dcd12095e
Rami Kass Youssef <rami.k.youssef@gmail.com>
2022-02-13 12:46:14 +0200

    uploaded download middleware

--- src/uploader/storage/provider.ts
+++ src/uploader/storage/provider.ts
@@ -1,36 +1,64 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { StorageFunction } from '.';
-import { UploadModuleOptions } from '..';
-import { UPLOAD_MODULE_OPTIONS } from '../consts';
-import { Response } from 'express';
-import { generateLocalStorageFunc } from './local-storage';
+import { Inject, Injectable } from "@nestjs/common";
+import { StorageFunction } from ".";
+import { UploadedFile, UploadModuleOptions } from "..";
+import { UPLOAD_MODULE_OPTIONS } from "../consts";
+import { Response } from "express";
+import { generateLocalStorageFunc } from "./local-storage";
 
-import * as fs from 'fs';
-import { FileNotFoundException } from '../exceptions';
+import * as fs from "fs";
+import * as archiver from "archiver";
+
+import { FileNotFoundException } from "../exceptions";
 
 @Injectable()
 export class StorageProvider {
   constructor(
-    @Inject(UPLOAD_MODULE_OPTIONS) private options: UploadModuleOptions,
+    @Inject(UPLOAD_MODULE_OPTIONS) private options: UploadModuleOptions
   ) {}
 
   getStorageFunc(): StorageFunction {
     return generateLocalStorageFunc(
-      this.options.localStorageOptions.storageDir ?? 'uploads',
-      this.options.localStorageOptions.publicServePath,
+      this.options.localStorageOptions.storageDir ?? "uploads",
+      this.options.localStorageOptions.publicServePath
     );
   }
 
-  pipePrivateFile(
-    res: Response,
-    file: { url?: string; path: string; name?: string },
-  ) {
-    const name = file.name ?? 'file';
+  pipePrivateFile(res: Response, file: UploadedFile) {
+    const name = file.originalName ?? "file";
     if (file.url) {
       // online
     } else {
       if (!fs.existsSync(file.path)) throw new FileNotFoundException();
       fs.createReadStream(file.path).pipe(res);
     }
+    res.setHeader("content-disposition", `attachment; filename=${name}`);
+  }
+
+  async zipPrivateFiles(
+    res: Response,
+    name: string,
+    files: Array<UploadedFile>
+  ) {
+    const archive = archiver("zip");
+    return new Promise((resolve, reject) => {
+      archive.on("error", function (err) {
+        reject(err);
+      });
+
+      files.forEach((fl) => {
+        if (fl.url) {
+          // online
+        } else {
+          if (fs.existsSync(fl.path)) {
+            archive.file(fl.path, { name: fl.originalName });
+          }
+        }
+      });
+
+      archive.pipe(res);
+      archive.finalize();
+      archive.on("close", resolve);
+      res.setHeader("content-disposition", `attachment; filename=${name}`);
+    });
   }
 }
